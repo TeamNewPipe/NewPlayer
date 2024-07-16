66b49e225a709df5d8b97e19f1d210786bd3e0c8
Christian Schabesberger <chris.schabesberger@mailbox.org>
2024-07-16 16:38:52 +0200

    rename PlayerFragment to VideoPlayerFragment

--- app/src/main/java/net/newpipe/newplayer/VideoPlayerFragment.kt
+++ app/src/main/java/net/newpipe/newplayer/VideoPlayerFragment.kt
@@ -27,24 +27,20 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.hilt.navigation.compose.hiltViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import net.newpipe.newplayer.model.VideoPlayerViewModel
 import net.newpipe.newplayer.model.VideoPlayerViewModelImpl
-import net.newpipe.newplayer.ui.VideoPlayerControllerUI
 import net.newpipe.newplayer.ui.VideoPlayerUI
 import net.newpipe.newplayer.ui.theme.VideoPlayerTheme
 
 @AndroidEntryPoint
-class PlayerFragment() : Fragment() {
+class VideoPlayerFragment() : Fragment() {
 
     private val viewModel: VideoPlayerViewModel by viewModels<VideoPlayerViewModelImpl>()
 
@@ -64,7 +60,7 @@ class PlayerFragment() : Fragment() {
             //insetsController.hide(WindowInsetsCompat.Type.systemBars())
         }
 
-        val view = inflater.inflate(R.layout.player_framgent, container, false)
+        val view = inflater.inflate(R.layout.video_player_framgent, container, false)
         val composeView = view.findViewById<ComposeView>(R.id.player_copose_view)
         val frameView = view.findViewById<FrameLayout>(R.id.frame_layout)
 
