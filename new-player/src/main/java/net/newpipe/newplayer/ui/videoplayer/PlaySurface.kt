e15f1d91eb8da9bd7ff135dd5dffc765901b9d48
Christian Schabesberger <chris.schabesberger@mailbox.org>
2024-11-01 23:26:32 +0100

    make content crop possible

--- new-player/src/main/java/net/newpipe/newplayer/ui/videoplayer/PlaySurface.kt
+++ new-player/src/main/java/net/newpipe/newplayer/ui/videoplayer/PlaySurface.kt
@@ -29,6 +29,8 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -36,6 +38,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -83,9 +86,21 @@ internal fun PlaySurface(
             ActualView(modifier.fillMaxSize(), player)
         } else { /* if(fitMode == ContentScale.CROP) */
             if (uiRatio <= contentRatio) {
-                ActualView(modifier.fillMaxHeight(), player)
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentWidth(unbounded = true, align = Alignment.CenterHorizontally)
+                ) {
+                    ActualView(modifier.fillMaxHeight(), player)
+                }
             } else {
-                ActualView(modifier.fillMaxWidth(), player)
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentHeight(unbounded = true, align = Alignment.CenterVertically)
+                ) {
+                    ActualView(modifier.fillMaxWidth(), player)
+                }
             }
         }
     }
