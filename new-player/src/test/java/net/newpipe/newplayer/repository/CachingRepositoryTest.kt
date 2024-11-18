f21d57df07085fba05725d96422e90796e5c943e
Nicholas Sala <salanicholas.14@gmail.com>
2024-11-18 12:15:48 +0100

    Improved CachingRepository tests by checking the cache mechanism

--- new-player/src/test/java/net/newpipe/newplayer/repository/CachingRepositoryTest.kt
+++ new-player/src/test/java/net/newpipe/newplayer/repository/CachingRepositoryTest.kt
@@ -1,5 +1,8 @@
 package net.newpipe.newplayer.repository
 
+import io.mockk.clearMocks
+import io.mockk.coVerify
+import io.mockk.spyk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
@@ -8,13 +11,13 @@ import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.Test
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.BeforeClass
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class CachingRepositoryTest {
-    val mockMediaRepository = MockMediaRepository()
-    val delayTestRepository = DelayTestRepository(mockMediaRepository, 100)
-    val repository = CachingRepository(delayTestRepository)
+    val mockMediaRepository = spyk(MockMediaRepository())
+    val repository = CachingRepository(mockMediaRepository)
 
     companion object {
         @JvmStatic
@@ -30,6 +33,11 @@ class CachingRepositoryTest {
         }
     }
 
+    @Before
+    fun resetSpy() {
+        clearMocks(mockMediaRepository)
+    }
+
     @Test
     fun getRepoInfo() {
         val repoInfo = repository.getRepoInfo()
@@ -39,8 +47,16 @@ class CachingRepositoryTest {
 
     @Test
     fun getMetaInfo() = runTest {
-        val metaInfo = repository.getMetaInfo("test")
+        val metaInfo = repository.getMetaInfo("item")
         assertEquals("Test title", metaInfo.title)
+    }
+
+    @Test
+    fun getMetaInfo_callActualRepositoryOnceForSameItem() = runTest {
+        repository.getMetaInfo("item")
+        repository.getMetaInfo("item")
+        repository.getMetaInfo("item")
+        coVerify (exactly = 1) { mockMediaRepository.getMetaInfo("item") }
     }
 
     @Test
@@ -53,6 +69,14 @@ class CachingRepositoryTest {
     }
 
     @Test
+    fun getStreams_callActualRepositoryOnceForSameItem() = runTest {
+        repository.getStreams("item")
+        repository.getStreams("item")
+        repository.getStreams("item")
+        coVerify (exactly = 1) { mockMediaRepository.getStreams("item") }
+    }
+
+    @Test
     fun getSubtitles_returnsCorrectList() = runTest {
         val subtitles = repository.getSubtitles("item")
         assertEquals(3, subtitles.size)
@@ -62,16 +86,40 @@ class CachingRepositoryTest {
     }
 
     @Test
+    fun getSubtitles_callActualRepositoryOnceForSameItem() = runTest {
+        repository.getSubtitles("item")
+        repository.getSubtitles("item")
+        repository.getSubtitles("item")
+        coVerify (exactly = 1) { mockMediaRepository.getSubtitles("item") }
+    }
+
+    @Test
     fun getPreviewThumbnail() = runTest {
         val previewThumbnail = repository.getPreviewThumbnail("item", 1000)
         assertNull(previewThumbnail)
     }
 
     @Test
-    fun getPreviewThumbnailInfo() = runTest {
-        val previewThumbnailInfo = repository.getPreviewThumbnailsInfo("item")
-        assertEquals(10, previewThumbnailInfo.count)
-        assertEquals(500, previewThumbnailInfo.distanceInMS)
+    fun getPreviewThumbnail_callActualRepositoryOnceForSameItem() = runTest {
+        repository.getPreviewThumbnail("item", 1000)
+        repository.getPreviewThumbnail("item", 1000)
+        repository.getPreviewThumbnail("item", 1000)
+        coVerify (exactly = 1) { mockMediaRepository.getPreviewThumbnail("item", 1000) }
+    }
+
+    @Test
+    fun getPreviewThumbnailsInfo() = runTest {
+        val previewThumbnailsInfo = repository.getPreviewThumbnailsInfo("item")
+        assertEquals(10, previewThumbnailsInfo.count)
+        assertEquals(500, previewThumbnailsInfo.distanceInMS)
+    }
+
+    @Test
+    fun getPreviewThumbnailsInfo_callActualRepositoryOnceForSameItem() = runTest {
+        repository.getPreviewThumbnailsInfo("item")
+        repository.getPreviewThumbnailsInfo("item")
+        repository.getPreviewThumbnailsInfo("item")
+        coVerify (exactly = 1) { mockMediaRepository.getPreviewThumbnailsInfo("item") }
     }
 
     @Test
@@ -84,8 +132,24 @@ class CachingRepositoryTest {
     }
 
     @Test
+    fun getChapters_callActualRepositoryOnceForSameItem() = runTest {
+        repository.getChapters("item")
+        repository.getChapters("item")
+        repository.getChapters("item")
+        coVerify (exactly = 1) { mockMediaRepository.getChapters("item") }
+    }
+
+    @Test
     fun getTimestampLink() = runTest {
         assertEquals("test/link", repository.getTimestampLink("item", 0))
+    }
+
+    @Test
+    fun getTimestampLink_callActualRepositoryOnceForSameItem() = runTest {
+        repository.getTimestampLink("item", 0)
+        repository.getTimestampLink("item", 0)
+        repository.getTimestampLink("item", 0)
+        coVerify (exactly = 1) { mockMediaRepository.getTimestampLink("item", 0) }
     }
 
     @Test
